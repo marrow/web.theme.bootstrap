17577855bc54efcbb742708258334e3d6979174a
Alice Bevan–McGregor <alice@gothcandy.com>
2015-12-04 16:18:41 -0500

    Explicit end is good in multi-funciton template modules.

--- web/theme/bootstrap/base.py
+++ web/theme/bootstrap/base.py
@@ -3,7 +3,7 @@
 : from cinje.std.html import page as page_, default_header, default_footer
 
 
-: def bootstrap_header title, *, metadata=[], styles=[], scripts=[]
+: def bootstrap_header title, metadata=[], styles=[], scripts=[]
 	: """Prepare and generate the HTML <head> section."""
 		
 	: use default_header title, metadata=metadata, styles=styles, scripts=scripts
@@ -15,10 +15,11 @@
 : end
 
 
-: def page title, *, header=bootstrap_header, footer=default_footer, metadata=[], styles=[], scripts=[], **attributes
+: def page title, header=bootstrap_header, footer=default_footer, metadata=[], styles=[], scripts=[], **attributes
 	: """A general HTML page."""
 	
 	: scripts = ['//cdn.jsdelivr.net/g/jquery@1.11.3,bootstrap@3.3.5(js/bootstrap.js+js/bootstrap.min.js),fitvids@1.1.0'] + scripts
 	: styles = ['//cdn.jsdelivr.net/fontawesome/4.4.0/css/font-awesome.min.css', '//cdn.jsdelivr.net/g/bootstrap@3.3.5(css/bootstrap.min.css+css/bootstrap-theme.min.css)'] + styles
 	
-	: return page_(title, header=header, footer=footer, metadata=metadata, styles=styles, scripts=scripts)
+	: return page_(title, header=header, footer=footer, metadata=metadata, styles=styles, scripts=scripts, **attributes)
+: end
