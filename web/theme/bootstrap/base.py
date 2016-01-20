c28252aa03559fef22b1ae90b8400b54132edf6e
Alice Bevan–McGregor <alice@gothcandy.com>
2016-01-20 02:09:52 -0500

    Added jquery.cookie as dep.

--- web/theme/bootstrap/base.py
+++ web/theme/bootstrap/base.py
@@ -18,7 +18,7 @@
 : def page title, header=bootstrap_header, footer=default_footer, metadata=[], styles=[], scripts=[], **attributes
 	: """A general HTML page."""
 	
-	: scripts = ['//cdn.jsdelivr.net/g/jquery@1.11.3,bootstrap@3.3.5(js/bootstrap.js+js/bootstrap.min.js),fitvids@1.1.0'] + scripts
+	: scripts = ['//cdn.jsdelivr.net/g/jquery@1.11.3,bootstrap@3.3.5(js/bootstrap.js+js/bootstrap.min.js),jquery.cookie@1.4.1,fitvids@1.1.0'] + scripts
 	: styles = ['//cdn.jsdelivr.net/fontawesome/4.4.0/css/font-awesome.min.css', '//cdn.jsdelivr.net/g/bootstrap@3.3.5(css/bootstrap.min.css+css/bootstrap-theme.min.css)'] + styles
 	
 	: return page_(title, header=header, footer=footer, metadata=metadata, styles=styles, scripts=scripts, **attributes)
